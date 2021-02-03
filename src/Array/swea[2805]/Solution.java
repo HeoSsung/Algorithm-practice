10363b142a7f31c02c4ee12205155ea8ca07a009
Heosungwon <78368797+HeoSsung@users.noreply.github.com>
2021-02-04 01:18:47 +0900

    Update Solution.java

--- src/Array/swea[2805]/Solution.java
+++ src/Array/swea[2805]/Solution.java
@@ -1,43 +1,37 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
- 
+
 public class Solution {
- 
-    static int T, N;
-    static int[][] farm;
- 
-    public static void main(String[] args) throws Exception{
-         
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        T = Integer.parseInt(br.readLine());
-         
-        for(int t=1; t<=T ; t++) {
-            N = Integer.parseInt(br.readLine());    
-            farm = new int[N][N];
-            for(int i=0;i<N;i++) {
-                char[] input = br.readLine().toCharArray();
-                for(int j=0;j<N;j++) {
-                    farm[i][j] = input[j]-'0';
-                }
-            }
-            int value=0;
-            int skip=0;
-            int center = N/2;
-            int startX, endX;
-            for(int i=0;i<N;i++) {
-                startX = center -skip;
-                endX = center+skip;
-                for(int j= startX;j<=endX;j++)
-                    value+=farm[i][j];
-                if(i<center) {
-                    skip++;
-                }else
-                    skip--;
-            }
-             
-            System.out.println("#"+t+" "+value);
-        }
-         
-    }
+
+	static int T, N;
+  
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		T = Integer.parseInt(br.readLine());
+
+		for (int t = 1; t <= T; t++) {
+			N = Integer.parseInt(br.readLine());
+			int value = 0;
+			int skip = 0;
+			int center = N / 2;
+			int startX, endX;
+			for (int i = 0; i < N; i++) {
+				char[] input = br.readLine().toCharArray();
+				startX = center - skip;
+				endX = center + skip;
+				for (int j = startX; j <= endX; j++) {
+					value += input[j]-'0';
+				}
+				if (i < center)
+					skip++;
+				else
+					skip--;
+			}
+
+			System.out.println("#" + t + " " + value);
+		}
+
+	}
 }
