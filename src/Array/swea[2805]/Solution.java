ff8e61012c32e515822c71ce3ca4a226de071e1b
Heosungwon <78368797+HeoSsung@users.noreply.github.com>
2021-02-04 01:13:12 +0900

    swea[2805] 농작물 수확하기

--- src/Array/swea[2805]/Solution.java
+++ src/Array/swea[2805]/Solution.java
@@ -1,48 +1,43 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
+ 
 public class Solution {
-
-	static int T, N;
-	static int[][] farm;
-	static int value;
-
-	public static void main(String[] args) throws Exception{
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		T = Integer.parseInt(br.readLine());
-		
-		for(int t=1; t<=T ; t++) {
-			N = Integer.parseInt(br.readLine());	
-			farm = new int[N][N];
-			for(int i=0;i<N;i++) {
-				char[] input = br.readLine().toCharArray();
-				for(int j=0;j<N;j++) {
-					farm[i][j] = input[j]-'0';
-				}
-			}
-			value=0;
-			int skip=N/2;
-			int start = 0;
-			int end = 0;
-			for(int i=0;i<N;i++) {
-				if(i<=skip) {
-					start = skip-i;
-					end = skip+i;
-					for(int j=start;j<=end;j++)
-						value += farm[i][j];
-				}
-				else {
-					start++;
-					end--;
-					for(int j=start;j<=end;j++)
-						value += farm[i][j];
-				}
-			}
-			
-			System.out.println("#"+t+" "+value);
-		}
-		
-	}
+ 
+    static int T, N;
+    static int[][] farm;
+ 
+    public static void main(String[] args) throws Exception{
+         
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        T = Integer.parseInt(br.readLine());
+         
+        for(int t=1; t<=T ; t++) {
+            N = Integer.parseInt(br.readLine());    
+            farm = new int[N][N];
+            for(int i=0;i<N;i++) {
+                char[] input = br.readLine().toCharArray();
+                for(int j=0;j<N;j++) {
+                    farm[i][j] = input[j]-'0';
+                }
+            }
+            int value=0;
+            int skip=0;
+            int center = N/2;
+            int startX, endX;
+            for(int i=0;i<N;i++) {
+                startX = center -skip;
+                endX = center+skip;
+                for(int j= startX;j<=endX;j++)
+                    value+=farm[i][j];
+                if(i<center) {
+                    skip++;
+                }else
+                    skip--;
+            }
+             
+            System.out.println("#"+t+" "+value);
+        }
+         
+    }
 }
