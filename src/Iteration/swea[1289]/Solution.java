62eb701e7e1688fe840474b395fdff8378264d00
Heosungwon <78368797+HeoSsung@users.noreply.github.com>
2021-02-03 09:57:39 +0900

    Update Solution.java

--- src/Iteration/swea[1289]/Solution.java
+++ src/Iteration/swea[1289]/Solution.java
@@ -1,38 +1,39 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
  
+// Scanner --> BufferedReader
+// int[] --> char[]
+// local --> static
  
 public class Solution {
  
+    static int T;
+    static char[] input, memory;
  
- 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
-        int res;
-        StringBuilder sb = new StringBuilder();
-         
-        for (int t = 0; t < T; t++) {
-            res = 0;
-            String str = br.readLine();
-            int len = str.length();
-            char[] input = str.toCharArray();
-             
  
-            for (int i = 0; i < len; i++) {
-                if (input[i] =='1') {
-                    for (int j = i; j < len; j++) {
-                        if (input[j] == '0')
-                            input[j] = '1';
-                        else
-                            input[j] = '0';
+        T = Integer.parseInt(br.readLine());
+ 
+        for (int t = 1; t <= T; t++) {
+            int count = 0;
+            input = br.readLine().toCharArray();
+            memory = new char[input.length];
+ 
+            int cnt = memory.length;
+            for (int i = 0; i < cnt; i++)
+                memory[i] = '0';
+         
+            for (int i = 0; i < cnt; i++) {
+                if (memory[i] != input[i]) {
+                    for (int j = i; j < cnt; j++) {
+                        memory[j] = input[i];
                     }
-                    res++;
+ 
+                    count++;
                 }
             }
-            sb.append("#").append((t+1)).append(" ").append(res).append("\n");
+            System.out.println("#" + t + " " + count);
         }
-        System.out.println(sb);
     }
- 
 }
